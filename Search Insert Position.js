3732e689690102c1ec42a898e88ea12848c2d881
Mayar Abuzahra <79359366+Mayar-Abuzahra@users.noreply.github.com>
2023-03-20 22:46:44 +0200

    Update Search Insert Position.js

--- Search Insert Position.js
+++ Search Insert Position.js
@@ -5,7 +5,7 @@
  */
 var searchInsert = function(nums, target) {
     for(let i=0; i<nums.length; i++){
-      if(nums[i] == target || target<nums[i]){
+      if(nums[i] == target || target < nums[i]){
         return i
       }
     } 
