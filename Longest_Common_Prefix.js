eb8b228a77d9539b4941faa8df5dee5e5fc405c9
Mayar Abuzahra <79359366+Mayar-Abuzahra@users.noreply.github.com>
2023-03-21 14:37:05 +0200

    Add files via upload

---
+++ Longest_Common_Prefix.js
@@ -0,0 +1,28 @@
+var longestCommonPrefix = function(strs) {
+  let max_sub_string_prefix = "";
+  let j = 0;
+  let k = strs.length - 1;
+  let flag = 1;
+  let minimum_string_length = strs.reduce(function(a, b) {
+    return a.length <= b.length ? a : b;
+  }).length;
+  for(let i=1; i<=minimum_string_length; i++){
+    if(strs[j].slice(0, i) === strs[k].slice(0, i)){
+      while(j<k){
+        j++;
+        if(strs[j].slice(0, i) !== strs[k].slice(0, i)){
+          flag = 0
+          break;
+        }
+        k--;
+      }
+      if(flag !== 0)
+        max_sub_string_prefix = strs[0].slice(0, i);
+    } else {
+      break;
+    }
+    j = 0;
+    k = strs.length - 1;
+  }
+  return max_sub_string_prefix;
+};
